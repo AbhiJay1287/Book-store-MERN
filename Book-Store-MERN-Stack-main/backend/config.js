a6ce5bd13232e913ae2fd10ed78de26bf9cee132
ABHINAV GUMPU <105973197+AbhiJay1287@users.noreply.github.com>
2024-06-02 14:16:49 +0530

    Add files via upload

---
+++ Book-Store-MERN-Stack-main/backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://abhinav:<abcd1234>@abhinav.oezssyz.mongodb.net/';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
